840b69527305785cf323c83e31be82af80ef0042
mbmahdiiii <mbmahdi650@gmail.com>
2016-02-05 15:50:09 +0330

    Update info_fa.lua

--- plugins/info_fa.lua
+++ plugins/info_fa.lua
@@ -57,7 +57,6 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@GPMod Team'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
 	send_msg(extra.receiver, extra.user..' نام کاربری مورد نظر یافت نشد.', ok_cb, false)
