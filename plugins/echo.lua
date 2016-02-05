919dce6826736993f3aa153356dfaaaf4d957452
mbmahdiiii <mbmahdi650@gmail.com>
2016-02-05 17:55:01 +0330

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -1,20 +1,19 @@
-1	
-2	local function run(msg, matches)
-3	  local text = matches[1]
-4	  local b = 1
-5	
-6	  while b ~= 0 do
-7	    text = text:trim()
-8	    text,b = text:gsub('^!+','')
-9	  end
-10	  return text
-11	end
-12	
-13	return {
-14	  description = "Simplest plugin ever!",
-15	  usage = "!echo [whatever]: echoes the msg",
-16	  patterns = {
-17	    "^!echo +(.+)$"
-18	  }, 
-19	  run = run 
-20	}
+ocal function run(msg, matches)
+local text = matches[1]
+local b = 1
+
+while b ~= 0 do
+text = text:trim()
+text,b = text:gsub('^!+','')
+end
+return text
+end
+
+return {
+description = "Simplest plugin ever!",
+usage = "!echo [whatever]: echoes the msg",
+patterns = {
+"^!echo +(.+)$"
+}, 
+run = run 
+}
