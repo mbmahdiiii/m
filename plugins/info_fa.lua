ad100d454193c0987be2e81cde7efe9f82eff0ef
mbmahdiiii <mbmahdi650@gmail.com>
2016-02-05 15:42:20 +0330

    Update info_fa.lua

--- plugins/info_fa.lua
+++ plugins/info_fa.lua
@@ -96,7 +96,6 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@GPMod Team'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'ایدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
